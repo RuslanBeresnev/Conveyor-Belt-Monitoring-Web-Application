6c412a0e2c82dc852db9bf5812b31bc35654fdaf
ruslan_beresnyov <rusber03102003@gmail.com>
2024-12-10 13:58:38 +0300

    Добавлена возможность отправки оповещений в Telegram

--- BeltMonitoringWebApplication/NotificationService.py
+++ BeltMonitoringWebApplication/NotificationService.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from telegram import Bot
+import requests
 from enum import Enum
 
 application = FastAPI()
+TELEGRAM_BOT_TOKEN = "<Bot token>"
+telegram_bot = Bot(token=TELEGRAM_BOT_TOKEN)
+
+
+def get_user_chat_id():
+    '''
+    Возвращает id чата с пользователем, который последним отправил сообщение боту
+    '''
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/getUpdates"
+    response = requests.get(url)
+    updates = response.json()
+    if updates["ok"] and len(updates) > 0:
+        return updates["result"][-1]["message"]["chat"]["id"]
+    return -1
 
 
 class NotificationMethod(Enum):
@@ -21,8 +37,11 @@ def get_application_info():
 
 
 @application.post("/notification_service")
-def send_notification(notification: Notification):
+async def send_notification(notification: Notification):
     if notification.method == NotificationMethod.telegram_notification:
+        # Обработать в try/catch
+        await telegram_bot.send_message(chat_id=get_user_chat_id(), text=notification.message)
         return {"status": "OK", "method": NotificationMethod.telegram_notification, "message": notification.message}
     elif notification.method == NotificationMethod.gmail_notification:
+        # Добавить способ для Gmail
         return {"status": "OK", "method": NotificationMethod.gmail_notification, "message": notification.message}
