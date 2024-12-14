52720e4c4bc1efff81e0edc5da31fe4885b610be
ruslan_beresnyov <rusber03102003@gmail.com>
2024-12-14 04:58:27 +0300

    Исправлены ошибки при отправке оповещений через Gmail API

--- BeltMonitoringWebApplication/NotificationService.py
+++ BeltMonitoringWebApplication/NotificationService.py
@@ -1,15 +1,15 @@
-import telegram.error
 from fastapi import FastAPI
 from pydantic import BaseModel
 from telegram import Bot
-import requests
-from base64 import urlsafe_b64encode
-from os.path import exists
+import telegram.error
 from email.mime.text import MIMEText
+import requests
+from os.path import exists
+from base64 import urlsafe_b64encode
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-import smtplib
+from googleapiclient.discovery import build
 import Data
 
 application = FastAPI()
@@ -30,16 +30,18 @@ def get_user_chat_id_in_telegram():
     return -1
 
 
-def authenticate_user_in_gmail():
-    """Аутентификация в Gmail с помощью OAuth 2.0"""
+def authenticate_and_get_credentials():
+    """Аутентификация в Gmail с помощью OAuth 2.0 (срок годности токена истекает через час после
+    первой аутентификации)"""
     credentials = None
     if exists("token.json"):
-        credentials = Credentials.from_authorized_user_file("token.json", Data.GOOGLE_SCOPES)
+        credentials = Credentials.from_authorized_user_file(filename="token.json", scopes=Data.GOOGLE_SCOPES)
     if not credentials or not credentials.valid:
         if credentials and credentials.expired and credentials.refresh_token:
             credentials.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(Data.GOOGLE_CLIENT_SECRET_FILE, Data.GOOGLE_SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file=Data.GOOGLE_CLIENT_SECRET_FILE,
+                                                             scopes=Data.GOOGLE_SCOPES)
             credentials = flow.run_local_server(port=0)
         with open("token.json", "w") as token:
             token.write(credentials.to_json())
@@ -63,6 +65,7 @@ def get_application_info():
 
 @application.post("/notification_service/with_telegram")
 async def send_telegram_notification(notification: TelegramNotification):
+    # Добавить правильный возврат кодов ошибок при ошибках (а не просто оставить 200)
     user_chat_id = get_user_chat_id_in_telegram()
     if user_chat_id == -1:
         return {"status": "ERROR", "method": "telegram_notification", "error_message": "Chat with user not found"}
@@ -75,23 +78,13 @@ async def send_telegram_notification(notification: TelegramNotification):
 
 @application.post("/notification_service/with_gmail")
 def send_gmail_notification(notification: GmailNotification):
-    mail = MIMEText(notification.message)
-    mail["from"] = Data.FROM_EMAIL
-    mail["to"] = notification.to_email
-    mail["subject"] = notification.subject
+    message = MIMEText(notification.message)
+    message["to"] = notification.to_email
+    message["subject"] = notification.subject
+    formatted_message = {'raw': urlsafe_b64encode(message.as_bytes()).decode()}
 
-    formatted_mail = urlsafe_b64encode(mail.as_bytes()).decode()
-    smtp_server = smtplib.SMTP("smtp.gmail.com", 587)
-    smtp_server.starttls()
-    smtp_server.ehlo()
-
-    credentials = authenticate_user_in_gmail()
-    smtp_server.docmd(
-        "AUTH XOAUTH2",
-        f"user={Data.FROM_EMAIL}\x01auth=Bearer {credentials.token}\x01\x01"
-    )
+    credentials = authenticate_and_get_credentials()
+    gmail_service = build(serviceName="gmail", version="v1", credentials=credentials)
     # Добавить обработку ошибок
-    smtp_server.sendmail(Data.FROM_EMAIL, mail["to"], formatted_mail)
-
-    smtp_server.quit()
+    sent_message = gmail_service.users().messages().send(userId="me", body=formatted_message).execute()
     return {"status": "OK", "method": "gmail_notification", "message": notification.message}
