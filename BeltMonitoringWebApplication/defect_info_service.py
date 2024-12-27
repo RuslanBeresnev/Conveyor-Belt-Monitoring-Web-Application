043b518479657beb9354955a5577ca98b41a7205
ruslan_beresnyov <rusber03102003@gmail.com>
2024-12-28 02:02:37 +0300

    Добавлен эндпоинт для получения дефектов определённого типа
    - Также функционал формирования ответа с полной информацией о дефекте вынесен в отдельный метод form_response_model_from_defect()

--- BeltMonitoringWebApplication/defect_info_service.py
+++ BeltMonitoringWebApplication/defect_info_service.py
@@ -4,7 +4,7 @@ from sqlmodel import Session, select
 from base64 import b64encode
 from datetime import datetime
 from database_connection import engine
-from db_models import Defect
+from db_models import Defect, DefectType
 
 router = APIRouter(prefix="/defect_info", tags=["Defects Information Service"])
 
@@ -24,29 +24,50 @@ class DefectResponseModel(BaseModel):
     base64_photo: str  # from Photo model (converted to base64 format)
 
 
+def form_response_model_from_defect(defect: Defect):
+    """
+    Create DefectResponseModel from Defect DB model using sqlmodel Relationship class and other DB models
+    """
+    response = DefectResponseModel(
+        id=defect.id,
+        timestamp=defect.base_object.time,
+        type=defect.type_object.name,
+        is_on_belt=defect.type_object.is_belt,
+        box_width_in_mm=defect.box_width,
+        box_length_in_mm=defect.box_length,
+        longitudinal_position=defect.location_length_in_conv,
+        transverse_position=defect.location_width_in_conv,
+        probability=defect.probability,
+        is_critical=defect.is_critical,
+        is_extreme=defect.is_extreme,
+        base64_photo=b64encode(defect.photo_object.image).decode()
+    )
+    return response
+
+
 @router.get("/")
 def get_service_info():
     return {"info": "Service providing information about emerging defects, their types and other useful parameters"}
 
 
-@router.get("/id={defect_id}", response_model=DefectResponseModel)
+@router.get(path="/id={defect_id}", response_model=DefectResponseModel)
 def get_defect_by_id(defect_id: int):
     with Session(engine) as session:
         defect = session.exec(select(Defect).where(Defect.id == defect_id)).first()
         if not defect:
             raise HTTPException(status_code=404, detail=f"There is no defect with id={defect_id}")
-        response = DefectResponseModel(
-            id=defect.id,
-            timestamp=defect.base_object.time,
-            type=defect.type_object.name,
-            is_on_belt=defect.type_object.is_belt,
-            box_width_in_mm=defect.box_width,
-            box_length_in_mm=defect.box_length,
-            longitudinal_position=defect.location_length_in_conv,
-            transverse_position=defect.location_width_in_conv,
-            probability=defect.probability,
-            is_critical=defect.is_critical,
-            is_extreme=defect.is_extreme,
-            base64_photo=b64encode(defect.photo_object.image).decode()
-        )
+        response = form_response_model_from_defect(defect)
+        return response
+
+
+@router.get(path="/type={defect_type}", response_model=list[DefectResponseModel])
+def get_defects_of_certain_type(defect_type: str):
+    with Session(engine) as session:
+        results = session.exec(select(Defect, DefectType).join(DefectType).
+                               where(DefectType.name == defect_type)).all()
+        if not results:
+            raise HTTPException(status_code=404, detail=f"There is no defects of type '{defect_type}'")
+        response = []
+        for defect, defect_type in results:
+            response.append(form_response_model_from_defect(defect))
         return response
